5c43dc1390dd3bcaace91834ffe2f1d2a3554ba2
zhou <164041601@qq.com>
2019-08-15 15:40:38 +0800

    一次大的优化

--- 纤维检验局门户网站/js/index.js
+++ 纤维检验局门户网站/js/index.js
@@ -23,3 +23,43 @@ function showAuto() {
     n = n >= (count - 1) ? 0 : ++n;
     $("#banner li").eq(n).trigger('click');
 }
+
+//选项卡
+$(".xjyx_btn").on("click", function () {
+    $(".xjyx_btn").not($(this)).attr("id", "");
+    if ($("#xxksj")) {
+        $("#xxksj").remove();
+    }
+    $(this).attr("id", "xjyx_btn_active");
+    var html = `<div class="sanjiao" id="xxksj"><img src="images/sanjiao.png" /></div>`
+    $(this).append($(html));
+
+
+    //切换右边对应数据
+    var list = `
+    <div class="news_content_list">
+        <dv class="news_content_list_r">
+            <div class="news_content_list_r_tit">工会第二小组开展“徒步康道舒身心 健康生活走起来”主题登山活动
+            </div>
+            <div class="news_content_list_r_msg">
+                近日，为丰富会员的业余文化生活，倡导绿色、健康的生活方式，在局工会的关心指导下，工会第二小组开展了“徒步康道舒身心健康生活走起来”主题登山活动。
+            </div>
+            <div class="xiangxi  visible-md-block visible-lg-block">[ 详细 ]</div>
+            <div class="clear"></div>
+        </dv>
+        <div class="xwimg"><img src="images/xwimg_05.png" /></div>
+    </div>
+    <div class="clear"></div>
+    `;
+    //装list的box
+    let news_box = $("#xxk_content");
+    news_box.empty();
+    //获取到的数据，只取前三条。
+    let data  = [{},{},{},{}];
+    for(let i = 0 ; i < 3 ; i++){
+        news_box.append($(list));
+        
+    }
+})
+//初始化 第一次肯定要显示数据吧
+$($(".xjyx_btn")[0]).trigger("click")
--- 纤维检验局门户网站/js/ini.js
+++ 纤维检验局门户网站/js/ini.js
@@ -1,102 +1,117 @@
 
-    //初始化宽度
-    function be() {
-        document.documentElement.style.fontSize = document.documentElement.clientWidth / 19.20 + "px";
-        if ($(window).width() > 992) {
-            $("#banner_list img").css({
-                "width": $("#banner").width() + "px",
-                "height": $(".c_r_t_last").height() + "px",
-            });
-            $(".news_lists").css({
-                "height": $("#banner").outerHeight() + "px",
-            });
-            $(".news_list_content").css({
-                "height": $(".news_list_tab").height() + "px",
-            });
-            $(".c_r_t_sec > .row").css({
-               "height": ($(".content_right").height() - $(".c_r_tit").height() - $(".c_r_t_last").height() - 10) + "px"
-            });
-
-            //设置main高度
-            var main_height = $(window).height() - 60 - $("#pcheader").height() - 20;
-            $("#main").css("height",main_height);
-        } else {
-            $(".news_lists").css({
-                "height": "auto"
-            });
-
-            $(".c_r_t_sec > .row").css({
-                "height":"auto"
-             });
-            $(".news_list_content").css({
-                "height": "auto"
-            });
-            $("#banner_list img").css({
-                "width": $("#banner").width() + "px",
-                "height": $("#banner_box").height() + "px",
-            });
-                //设置main高度
-                var main_height = $(window).height();
-                $("#main").css("height",main_height);
-        }
-
-    
-    }
-    be();
-    $(window).resize(function () {
-        be();
-    });
-    
-    
-
-
-
-
-
-
-    //右侧菜单事件
-        $("#menubtn").hover(function(){
-            let thiz = this;
-            $("#right_abso").animate({
-                right:"0px"
-            },200,function(){
-                $(thiz).css("cssText","display: none!important");
-            });
-        })
-
-
-        $("#right_abso").hover(function(){
-            
-        },function(){
-            $("#right_abso").animate({
-                right:"-100%"
-            },200,function(){
-                $("#menubtn").css("cssText","display: block!important");
-            });
+//初始化宽度
+function be() {
+    document.documentElement.style.fontSize = document.documentElement.clientWidth / 19.20 + "px";
+    if ($(window).width() > 992) {
+        $("#banner_list img").css({
+            "width": $("#banner").width() + "px",
+            "height": $(".c_r_t_last").height() + "px",
+        });
+        $(".news_lists").css({
+            "height": $("#banner").outerHeight() + "px",
+        });
+        $(".news_list_content").css({
+            "height": $(".news_list_tab").height() + "px",
+        });
+        $(".c_r_t_sec > .row").css({
+            "height": ($(".content_right").height() - $(".c_r_tit").height() - $(".c_r_t_last").height() - 10) + "px"
         });
 
+        //设置main高度
+        var main_height = $(window).height() - 60 - $("#pcheader").height() - 20;
+        $("#main").css("height", main_height);
+    } else {
+        $(".news_lists").css({
+            "height": "auto"
+        });
+
+        $(".c_r_t_sec > .row").css({
+            "height": "auto"
+        });
+        $(".news_list_content").css({
+            "height": "auto"
+        });
+        $("#banner_list img").css({
+            "width": $("#banner").width() + "px",
+            "height": $("#banner_box").height() + "px",
+        });
+        //设置main高度
+        var main_height = $(window).height();
+        $("#main").css("height", main_height);
+    }
 
 
-
-
-
-
-
-
-    //手机菜单
-    $("#m_menubtn").on("click",function(){
-        let ele = $("#mobile_nav_box");
-        let zt = ele.attr("zt");
-        if(zt == "0"){
-            ele.animate({
-                left:"0px"
-            });
-            ele.attr("zt","1");
-        }else{
-            ele.animate({
-                left:"-100%"
-            });
-            ele.attr("zt","0");
-
-        }
+}
+be();
+$(window).resize(function () {
+    be();
+});
+//右侧菜单事件
+$("#menubtn").hover(function () {
+    let thiz = this;
+    $("#right_abso").animate({
+        right: "0px"
+    }, 200, function () {
+        $(thiz).css("cssText", "display: none!important");
     });
+})
+
+
+$("#right_abso").hover(function () {
+
+}, function () {
+    $("#right_abso").animate({
+        right: "-100%"
+    }, 200, function () {
+        $("#menubtn").css("cssText", "display: block!important");
+    });
+});
+//手机菜单
+$("#m_menubtn").on("click", function () {
+    let ele = $("#mobile_nav_box");
+    let zt = ele.attr("zt");
+    if (zt == "0") {
+        ele.animate({
+            left: "0px"
+        });
+        ele.attr("zt", "1");
+    } else {
+        ele.animate({
+            left: "-100%"
+        });
+        ele.attr("zt", "0");
+
+    }
+});
+
+//二级页面弹出层
+//关闭
+$("[guanbi_sec_btn]").on("click", function () {
+    if ($(window).width() > 992) {
+        //弹出层外壳
+        var sec_page = $("#sec_page");
+        sec_page.animate({
+            top: "100%",
+        });
+    } else {
+        $("#sec_page_pc").animate({
+            top: "100%",
+        });
+    }
+
+
+});
+
+$("[sec_page]").on("click", function () {
+    if ($(window).width() > 992) {
+           //弹出层外壳
+        var sec_page = $("#sec_page");
+        sec_page.animate({
+            top: "0px",
+        });
+    } else {
+        $("#sec_page_pc").animate({
+            top: "0px",
+        });
+    }
+});
